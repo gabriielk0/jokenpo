6b44d6b3d1b54136e867724a34fbc503fee2f5ef
Gabriel <oliveiracgabriel052@gmail.com>
2024-02-16 22:10:29 -0300

    .

---
+++ app/sorteio.js
@@ -0,0 +1,75 @@
+function gerarNumero () {
+    const maiorValor = 9;
+    return parseInt(Math.random() * maiorValor + 1);
+}
+
+
+
+function chuteComputador() {
+    const papelComp = [1, 4, 7];
+    const pedraComp = [2, 5, 8];
+    const numero = gerarNumero();
+
+    if (papelComp.includes(numero)) {
+        comptOpcao('./assets/img/papel_direita.png');
+        console.log('papel')
+        console.log('numero:',numero)
+    } else if (pedraComp.includes(numero)){
+        comptOpcao('./assets/img/pedra_direita.png');
+        console.log('pedra')
+        console.log('numero:', numero)
+    } else {
+        comptOpcao('./assets/img/tesoura_direita.png');
+        console.log('tesoura')
+        console.log('numero:',numero)
+    }
+
+    switch(botaoClicado) {
+        case 'papel':
+            if (papelComp.includes(numero)) {
+                console.log('Empate')
+                console.log('---------')
+            } else if(pedraComp.includes(numero)){
+                console.log('Você ganhou')
+                console.log('---------')
+            } else {
+                console.log('Você perdeu!')
+                console.log('---------')
+            }
+
+        case 'pedra':
+            if (papelComp.includes(numero)) {
+                console.log('Você perdeu!')
+                console.log('---------')
+            } else if(pedraComp.includes(numero)){
+                console.log('Empate')
+                console.log('---------')
+            } else {
+                console.log('Você Ganhou!')
+                console.log('---------')
+            }
+
+        case 'tesoura':
+            if (papelComp.includes(numero)) {
+                console.log('Você ganhou')
+                console.log('---------')
+            } else if(pedraComp.includes(numero)){
+                console.log('Você perdeu')
+                console.log('---------')
+            } else {
+                console.log('Empate')
+                console.log('---------')
+            }
+    }
+}
+
+function comptOpcao (caminho) {
+    opcaoComp.src = (caminho);
+}
+
+function opcaoBtnUsuario (caminho) {
+    playUsuario.src = caminho;
+    chuteComputador();
+}
+
+
